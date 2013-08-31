359890ab0c5ade6bf303d92528bc4247da794afe
Herman Schaaf <hermanschaaf@gmail.com>
2013-08-31 21:14:41 +0900

    Add past tense verbs
    Also updated functions to return Word objects, not strings
    Update tests to do the same

--- words.go
+++ words.go
@@ -14,8 +14,25 @@ func (w *Word) Print() {
 	fmt.Println(w.kana, w.kanji)
 }
 
+func (w *Word) GetWord() Word {
+	return *w
+}
+
 func (w *Word) GetRoot() string {
 	return w.kana
+}
+
+func (w *Word) GetLastKana() string {
+	// get the last kana character in a word
+	kanaRune := []rune(w.kana)
+	return string(kanaRune[len(kanaRune)-1:])
+}
+
+func (w *Word) GetAllButLast() (kanji, kana string) {
+	// get all but the last character of a word
+	kanjiRune := []rune(w.kanji)
+	kanaRune := []rune(w.kana)
+	return string(kanjiRune[:len(kanjiRune)-1]), string(kanaRune[:len(kanaRune)-1])
 }
 
 /* === VERBS === */
@@ -28,54 +45,101 @@ type RuVerb struct {
 	Verb
 }
 
-func (v *RuVerb) Negative() string {
-	// drop the る and attach ない
-	kanjiRune := []rune(v.kanji)
+func (v *RuVerb) Negative() Word {
 	// one exception for case "ある":
 	if v.kanji == "ある" {
-		return "ない"
+		return Word{"ない", "ない", "not " + v.english}
 	}
-	return string(kanjiRune[:len(kanjiRune)-1]) + "ない"
+	// drop the る and attach ない
+	restOfKanji, restOfKana := v.GetAllButLast()
+	return Word{restOfKanji + "ない", restOfKana + "ない", "not " + v.english}
+}
+
+func (v *RuVerb) Past() Word {
+	// drop the る and attach た
+	restOfKanji, restOfKana := v.GetAllButLast()
+	return Word{restOfKanji + "た", restOfKana + "た", "did " + v.english}
 }
 
 type UVerb struct {
 	Verb
 }
 
-func (v *UVerb) Negative() string {
-	kanjiRune := []rune(v.kanji)
-	lastCharacter := string(kanjiRune[len(kanjiRune)-1:])
-	restOfWord := string(kanjiRune[:len(kanjiRune)-1])
+func (v *UVerb) Negative() Word {
+	lastCharacter := v.GetLastKana()
+	restOfKanji, restOfKana := v.GetAllButLast()
 
 	// if verb ends in う, replace う with わない
 	if lastCharacter == "う" {
-		return restOfWord + "わない"
+		extra := "わない"
+		return Word{restOfKanji + extra, restOfKana + extra, "not " + v.english}
 		// otherwise replace with the -a equivalent
 	} else {
 		original := []string{"つ", "く", "ゅ", "す", "ぬ", "ふ", "む", "ゆ", "ぐ", "ず", "づ", "ぶ", "ぷ", "る"}
 		replace := []string{"た", "か", "ゃ", "さ", "な", "は", "ま", "や", "が", "ざ", "ざ", "ば", "ぱ", "ら"}
 		for i, o := range original {
 			if o == lastCharacter {
-				return restOfWord + replace[i] + "ない"
+				extra := replace[i] + "ない"
+				return Word{restOfKanji + extra, restOfKana + extra, "not " + v.english}
 			}
 		}
 	}
 	// return original word if all else fails
-	return v.kanji
+	return v.GetWord()
+}
+
+func (v *UVerb) Past() Word {
+	/*
+		Get the past-tense form of an U-verb
+	*/
+
+	lastCharacter := v.GetLastKana()
+	restOfKanji, restOfKana := v.GetAllButLast()
+
+	// handle exceptions first:
+	if v.kanji == "する" {
+		return Word{"した", "した", "did" + v.english}
+	}
+	if v.kanji == "くる" {
+		return Word{"きた", "きた", "did" + v.english}
+	}
+	// 行く is only an exception for this rule
+	if v.kanji == "行く" {
+		return Word{"行った", "いった", "did" + v.english}
+	}
+
+	switch lastCharacter {
+	case "す":
+		return Word{restOfKanji + "した", restOfKana + "した", "did " + v.english}
+	case "く":
+	case "ぐ":
+		return Word{restOfKanji + "いた", restOfKana + "いた", "did " + v.english}
+	case "む":
+	case "ぶ":
+	case "ぬ":
+		return Word{restOfKanji + "んだ", restOfKana + "んだ", "did " + v.english}
+	case "る":
+	case "う":
+	case "つ":
+		return Word{restOfKanji + "った", restOfKana + "った", "did " + v.english}
+	}
+
+	// otherwise we just return the same word, because we don't know what to do:
+	return v.GetWord()
 }
 
 type ExceptionVerb struct {
 	Verb
 }
 
-func (v *ExceptionVerb) Negative() string {
+func (v *ExceptionVerb) Negative() Word {
 	switch v.kanji {
 	case "する":
-		return "しない"
+		return Word{"しない", "しない", "not do"}
 	case "くる":
-		return "こない"
+		return Word{"こない", "こない", "not come"}
 	}
-	return v.kanji
+	return v.GetWord()
 }
 
 /* === ADJECTIVES === */
--- words_test.go
+++ words_test.go
@@ -11,65 +11,75 @@ func Test(t *testing.T) { TestingT(t) }
 type JapaneseSuite struct{}
 
 type TestWord struct {
-	root     string
-	negative string
+	kanji       string
+	kana        string
+	english     string
+	neg_kanji   string
+	neg_kana    string
+	neg_english string
 }
 
 var _ = Suite(&JapaneseSuite{})
 
 // ru-verbs
 var ruVerbs []TestWord = []TestWord{
-	TestWord{"見る", "見ない"},
-	TestWord{"食べる", "食べない"},
-	TestWord{"寝る", "寝ない"},
-	TestWord{"起きる", "起きない"},
-	TestWord{"考える", "考えない"},
-	TestWord{"教える", "教えない"},
-	TestWord{"出る", "出ない"},
-	TestWord{"着る", "着ない"},
-	TestWord{"いる", "いない"},
-	TestWord{"ある", "ない"},
+	TestWord{"見る", "みる", "see", "見ない", "みない", "not see"},
+	TestWord{"食べる", "たべる", "eat", "食べない", "たべない", "not eat"},
+	TestWord{"寝る", "ねる", "sleep", "寝ない", "ねない", "not sleep"},
+	TestWord{"起きる", "おきる", "get up", "起きない", "おきない", "not get up"},
+	TestWord{"考える", "かんがえる", "consider", "考えない", "かんがえない", "not consider"},
+	TestWord{"教える", "おしえる", "teach", "教えない", "おしえない", "not teach"},
+	TestWord{"出る", "でる", "go out", "出ない", "でない", "not go out"},
+	TestWord{"着る", "きる", "wear", "着ない", "きない", "not wear"},
+	TestWord{"いる", "いる", "be", "いない", "いない", "not be"},
+	TestWord{"ある", "ある", "be", "ない", "ない", "not be"},
 }
 
 // u-verbs
 var uVerbs []TestWord = []TestWord{
-	TestWord{"話す", "話さない"},
-	TestWord{"聞く", "聞かない"},
-	TestWord{"泳ぐ", "泳がない"},
-	TestWord{"遊ぶ", "遊ばない"},
-	TestWord{"待つ", "待たない"},
-	TestWord{"飲む", "飲まない"},
-	TestWord{"買う", "買わない"},
-	TestWord{"帰る", "帰らない"},
-	TestWord{"死ぬ", "死なない"},
+	TestWord{"話す", "はなす", "speak", "話さない", "はなさない", "not speak"},
+	TestWord{"聞く", "きく", "hear", "聞かない", "きかない", "not hear"},
+	TestWord{"泳ぐ", "およぐ", "swim", "泳がない", "およがない", "not swim"},
+	TestWord{"遊ぶ", "あそぶ", "play", "遊ばない", "あそばない", "not play"},
+	TestWord{"待つ", "まつ", "wait", "待たない", "またない", "not wait"},
+	TestWord{"飲む", "のむ", "drink", "飲まない", "のまない", "not drink"},
+	TestWord{"買う", "かう", "buy", "買わない", "かわない", "not buy"},
+	TestWord{"帰る", "かえる", "return", "帰らない", "かえらない", "not return"},
+	TestWord{"死ぬ", "しぬ", "die", "死なない", "しなない", "not die"},
 }
 
 // exceptions
 var exceptionVerbs []TestWord = []TestWord{
-	TestWord{"する", "しない"},
-	TestWord{"くる", "こない"},
+	TestWord{"する", "する", "do", "しない", "しない", "not do"},
+	TestWord{"くる", "くる", "come", "こない", "こない", "not come"},
 }
 
 func (s *JapaneseSuite) TestNegativeRuVerbs(c *C) {
 	// check that ru-verbs get the correct negative suffix
-	for _, ruVerb := range ruVerbs {
-		v := RuVerb{Verb{Word{ruVerb.root, "", ""}}}
-		c.Check(v.Negative(), Equals, ruVerb.negative)
+	for _, verb := range ruVerbs {
+		v := RuVerb{Verb{Word{verb.kanji, verb.kana, verb.english}}}
+		neg_word := v.Negative()
+		c.Check(neg_word.kanji, Equals, verb.neg_kanji)
+		c.Check(neg_word.kana, Equals, verb.neg_kana)
 	}
 }
 
 func (s *JapaneseSuite) TestNegativeUVerbs(c *C) {
 	// check that ru-verbs get the correct negative suffix
-	for _, uVerb := range uVerbs {
-		v := UVerb{Verb{Word{uVerb.root, "", ""}}}
-		c.Check(v.Negative(), Equals, uVerb.negative)
+	for _, verb := range uVerbs {
+		v := UVerb{Verb{Word{verb.kanji, verb.kana, verb.english}}}
+		neg_word := v.Negative()
+		c.Check(neg_word.kanji, Equals, verb.neg_kanji)
+		c.Check(neg_word.kana, Equals, verb.neg_kana)
 	}
 }
 
 func (s *JapaneseSuite) TestNegativeExceptionVerbs(c *C) {
 	// check that ru-verbs get the correct negative suffix
-	for _, exceptionVerb := range exceptionVerbs {
-		v := ExceptionVerb{Verb{Word{exceptionVerb.root, "", ""}}}
-		c.Check(v.Negative(), Equals, exceptionVerb.negative)
+	for _, verb := range exceptionVerbs {
+		v := ExceptionVerb{Verb{Word{verb.kanji, verb.kana, verb.english}}}
+		neg_word := v.Negative()
+		c.Check(neg_word.kanji, Equals, verb.neg_kanji)
+		c.Check(neg_word.kana, Equals, verb.neg_kana)
 	}
 }
