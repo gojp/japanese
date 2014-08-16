895657220181b441648c635deeb2bda0ff2073e0
Shawn Smith <shawnpsmith@gmail.com>
2014-08-16 13:59:07 +0900

    rename Rudeshou to Deshou, change tests style

--- identify_test.go
+++ identify_test.go
@@ -2,40 +2,45 @@ package japanese
 
 import "testing"
 
-func wantXandGotY(t *testing.T, got interface{}, want interface{}) {
-	if got != want {
-		t.Errorf("Expected %v, but got %v", got, want)
+func TestIdentifyForm(t *testing.T) {
+	tests := []struct {
+		give string
+		want int
+	}{
+		{"ある", PresentIndicative},
+		{"あります", PresentIndicative},
+		{"ない", PresentIndicative},
+		{"ありません", PresentIndicative},
+		{"あった", PastIndicative},
+		{"なかった", PastIndicative},
+		{"ありました", PastIndicative},
+		{"ありませんでした", PastIndicative},
 	}
-}
 
-func wantXgotYgivenZ(t *testing.T, got interface{}, want interface{}, give interface{}) {
-	if got != want {
-		t.Errorf("Expected f(%v) = %v, but got %v", give, got, want)
-	}
-}
-
-func TestIdentifyFormPresentIndicative(t *testing.T) {
-	verbs := []string{"ある", "あります", "ない", "ありません"}
-	for _, v := range verbs {
-		wantXandGotY(t, PresentIndicative, IdentifyForm(v))
-	}
-}
-
-func TestIdentifyFormPastIndicative(t *testing.T) {
-	verbs := []string{"あった", "なかった", "ありました", "ありませんでした"}
-	for _, v := range verbs {
-		wantXandGotY(t, PastIndicative, IdentifyForm(v))
+	for _, tt := range tests {
+		if got := IdentifyForm(tt.give); got != tt.want {
+			t.Errorf("IdentifyForm(%q) = %d, want %d", tt.give, got, tt.want)
+		}
 	}
 }
 
 func TestIdentifyPositivity(t *testing.T) {
-	verbs := []string{"ない", "ません", "なかった", "ませんでした"}
-	for _, v := range verbs {
-		wantXandGotY(t, Negative, IdentifyPositivity(v))
+	tests := []struct {
+		in   string
+		want int
+	}{
+		{"ある", Positive},
+		{"あります", Positive},
+		{"あった", Positive},
+		{"ありました", Positive},
+		{"ない", Negative},
+		{"ません", Negative},
+		{"なかった", Negative},
+		{"ませんでした", Negative},
 	}
-
-	verbs = []string{"ある", "あります", "あった", "ありました"}
-	for _, v := range verbs {
-		wantXandGotY(t, Positive, IdentifyPositivity(v))
+	for _, tt := range tests {
+		if got := IdentifyPositivity(tt.in); got != tt.want {
+			t.Errorf("IdentifyPositivity(%q) = %d, want %d", tt.in, got, tt.want)
+		}
 	}
 }
--- transform_test.go
+++ transform_test.go
@@ -3,55 +3,71 @@ package japanese
 import "testing"
 
 func TestSplitEnding(t *testing.T) {
-	verbs := []string{"ある", "あります", "ない", "ありません"}
-	roots := []string{"あ", "あり", "", "あり"}
-	ends := []string{"る", "ます", "ない", "ません"}
-	for i, v := range verbs {
-		root, ending := SplitEnding(v)
-		wantXandGotY(t, roots[i], root)
-		wantXandGotY(t, ends[i], ending)
+	tests := []struct {
+		verb   string
+		root   string
+		ending string
+	}{
+		{"ある", "あ", "る"},
+		{"あります", "あり", "ます"},
+		{"ない", "", "ない"},
+		{"ありません", "あり", "ません"},
+	}
+
+	for _, tt := range tests {
+		root, ending := SplitEnding(tt.verb)
+		if root != tt.root {
+			t.Errorf("SplitEnding(%q) root = %q, want %q", tt.verb, root, tt.root)
+		}
+		if ending != tt.ending {
+			t.Errorf("SplitEnding(%q) end = %q, want %q", tt.verb, ending, tt.ending)
+		}
 	}
 }
 
 func TestDictionaryFormBasicIchidans(t *testing.T) {
-	// ichidan verbs
-	verbs := []string{"たべます", "おしえます", "います"}
-	dicts := []string{"たべる", "おしえる", "いる"}
-	for i, v := range verbs {
-		_, ichidan := DictionaryForm(v)
-		wantXgotYgivenZ(t, dicts[i], ichidan, v)
+	tests := []struct {
+		in   string
+		want string
+	}{
+		{"たべます", "たべる"},
+		{"おしえます", "おしえる"},
+		{"います", "いる"},
+	}
+
+	for _, tt := range tests {
+		_, got := DictionaryForm(tt.in)
+		if got != tt.want {
+			t.Errorf("DictionaryForm(%q) ichidan = %q, want %q")
+		}
 	}
 }
 
 func TestDictionaryFormBasicGodans(t *testing.T) {
-	// godan verbs
-	verbs := []string{"いく", "ききます", "ぬぎます", "うたいます"}
-	dicts := []string{"いく", "きく", "ぬぐ", "うたう"}
-	for i, v := range verbs {
-		godan, _ := DictionaryForm(v)
-		wantXgotYgivenZ(t, dicts[i], godan, v)
+	tests := []struct {
+		in   string
+		want string
+	}{
+		{"いく", "いく"},
+		{"ききます", "きく"},
+		{"ぬぎます", "ぬぐ"},
+		{"うたいます", "うたう"},
+	}
+
+	for _, tt := range tests {
+		got, _ := DictionaryForm(tt.in)
+		if got != tt.want {
+			t.Errorf("DictionaryForm(%q) godan = %q, want %q")
+		}
 	}
 }
 
 func TestDictionaryFormAru(t *testing.T) {
 	verbs := []string{"ある", "あります", "ない", "ありません"}
 	for _, v := range verbs {
-		godan, _ := DictionaryForm(v)
-		wantXandGotY(t, "ある", godan)
-	}
-}
-
-func TestDictionaryFormIchidan(t *testing.T) {
-	verbs := []string{"たべます", "たべました", "たべた"}
-	for _, v := range verbs {
-		_, ichidan := DictionaryForm(v)
-		wantXandGotY(t, "たべる", ichidan)
-	}
-
-	verbs = []string{"開ける", "開けて", "開けます", "開けない", "開けません"}
-	for _, v := range verbs {
-		_, ichidan := DictionaryForm(v)
-		wantXandGotY(t, "開ける", ichidan)
+		if got, _ := DictionaryForm(v); got != "ある" {
+			t.Errorf("DictionaryForm(%q) godan = %q, want %q", v, got, "ある")
+		}
 	}
 }
 
@@ -60,21 +76,24 @@ func TestDictionaryFormIchidanAkeru(t *testing.T) {
 		"あけました", "あけませんでした", "あけたろう", "あけただろう", "あけなかっただろう", "あけたでしょう", "あけなかったでしょう"} //"あけている", "あけています", "あけていません", "あけていた", "あけていました", "あけていませんでした", "あければ", "あけなければ", "あけませば", "あけますれば", "あけませんなら", "あけたら", "あけなかったら", "あけましたら", "あけませんでしたら", "あけられる", "あけられない", "あけられます", "あけられません", "あけさせる", "あけさせない", "あけさせます", "あけさせません"}
 
 	for _, v := range verbs {
-		_, ichidan := DictionaryForm(v)
-		wantXgotYgivenZ(t, "あける", ichidan, v)
+		if _, got := DictionaryForm(v); got != "あける" {
+			t.Errorf("DictionaryForm(%q) ichidan = %q, want %q", v, got, "あける")
+		}
 	}
 }
 
-func TestDictionaryFormGodan(t *testing.T) {
+func TestDictionaryFormIchidan(t *testing.T) {
 	verbs := []string{"たべます", "たべました", "たべた"}
 	for _, v := range verbs {
-		_, ichidan := DictionaryForm(v)
-		wantXandGotY(t, "たべる", ichidan)
+		if _, got := DictionaryForm(v); got != "たべる" {
+			t.Errorf("DictionaryForm(%q) ichidan = %q, want %q", v, got, "たべる")
+		}
 	}
 
 	verbs = []string{"開ける", "開けて", "開けます", "開けない", "開けません"}
 	for _, v := range verbs {
-		_, ichidan := DictionaryForm(v)
-		wantXandGotY(t, "開ける", ichidan)
+		if _, got := DictionaryForm(v); got != "開ける" {
+			t.Errorf("DictionaryForm(%q) ichidan = %q, want %q", v, got, "開ける")
+		}
 	}
 }
